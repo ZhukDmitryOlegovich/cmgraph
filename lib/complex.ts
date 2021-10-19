f6ff461e573529740b5ba6995807f407b893ee5d
ZhukDima <zhukdo@gmail.com>
2021-10-19 16:23:19 +0300

    complex

--- lib/complex.ts
+++ lib/complex.ts
@@ -14,4 +14,26 @@ export class Complex {
 	sub({ r, i }: Complex): Complex {
 		return new Complex(this.r - r, this.i - i);
 	}
+
+	mul({ r, i }: Complex): Complex {
+		return new Complex(this.r * r - this.i * i, this.r * i + r * this.i);
+	}
+
+	div({ r, i }: Complex): Complex {
+		const sqr = r * r + i * i;
+		return new Complex((this.r * r + this.i * i) / sqr, (this.i * r - this.r * i) / sqr);
+	}
+
+	exp(): Complex {
+		const er = Math.exp(this.r);
+		return new Complex(er * Math.cos(this.i), er * Math.sin(this.i));
+	}
+
+	get length(): number {
+		return this.i * this.i + this.r * this.r;
+	}
+
+	get normalize(): Complex {
+		return this.div(new Complex(this.length));
+	}
 }
